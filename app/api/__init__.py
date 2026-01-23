8ab785fa10e6d256a51d4376de9ec33dc5245014
aiseeingvoice <aiseeingvoice@gmail.com>
2026-01-22 22:00:21 -0500

    update

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,16 +1,25 @@
-from fastapi import APIRouter, Depends
+import httpx, markdown
 
+from fastapi import APIRouter, Depends
+from fastapi.responses import HTMLResponse
 # from app.api.r_report import reportRou
+
 from app.api.r_wage_table import wageRou
 from app.api.r_embedding import router as embeddingRou
 
 rou = APIRouter()
+README_URL = "https://raw.githubusercontent.com/aiXpertLab/aiXpertLab/refs/heads/master/README.md"
+
+# @rou.get("/")
+# def rouGet():
+#     return {"queries": "Welcome to Wildfire!",}
 
 @rou.get("/")
-def rouGet():
-    return {"queries": "Welcome to Wildfire!",}
+async def rouGet():
+    async with httpx.AsyncClient() as client:
+        r = await client.get(README_URL)
 
-
+    return HTMLResponse(markdown.markdown(r.text))
 
 rou.include_router(wageRou, prefix="/wages", tags=["Wages"])
 rou.include_router(embeddingRou, prefix="/embeddings", tags=["Embeddings"])
